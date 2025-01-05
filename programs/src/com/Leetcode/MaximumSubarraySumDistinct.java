6488f712a425470caf427cffa08f1dc59e54b06b
Vivek Vishal <vishalvivek488@gmail.com>
2025-01-05 15:21:54 +0530

    Update MaximumSubarraySumDistinct.java

--- programs/src/com/Leetcode/MaximumSubarraySumDistinct.java
+++ programs/src/com/Leetcode/MaximumSubarraySumDistinct.java
@@ -32,4 +32,40 @@ class Solution {
     }
 }
 
+/*
+Optimization: instead of resetting j, increment i++ and then keep removing leftmost element from the set until duplicate is removed.
+
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        
+        long maxSum = 0;
+        long currSum = 0;
+        int i = 0;
+        //int j = 0;
+        HashSet<Integer> set = new HashSet<Integer>();
+
+        for (int j = 0; j < nums.length; j++) {
+            while (set.contains(nums[j])) {
+                set.remove(nums[i]);
+                currSum -= nums[i];
+                i++;
+            }
+
+            set.add(nums[j]);
+            currSum += nums[j];
+
+            if (j - i + 1 == k) {
+                maxSum = Math.max(maxSum, currSum);
+                currSum -= nums[i];
+                set.remove(nums[i]);
+                i++;
+            }
+        }
+        return maxSum;
+    }
+}
+
+
+
+*/
 
