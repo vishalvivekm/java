a52fa849b731425858041bb056827997d5c55d8d
Vivek Vishal <vishalvivek488@gmail.com>
2025-01-05 19:15:45 +0530

    Create ProductOfArrayExceptSelf.java

---
+++ programs/src/com/Leetcode/ProductOfArrayExceptSelf.java
@@ -0,0 +1,20 @@
+// LC 238
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ansArr = new int[n];
+
+        // my N-squred solution; meh
+
+        // for(int i = 0; i < ansArr.length; i++){
+        //     int ans = 1;
+        //     for(int j = 0; j < ansArr.length; j++){
+        //         if(j == i) continue;
+        //         ans = ans * nums[j];
+        //     }
+        //     ansArr[i] = ans;
+        // }
+
+        return ansArr;
+    }
+}
