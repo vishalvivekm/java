b7a7b7ea63ebe0900ea5f10a597d8398d4e66941
Vivek Vishal <vishalvivek488@gmail.com>
2025-01-05 19:16:31 +0530

    Update ProductOfArrayExceptSelf.java

--- programs/src/com/Leetcode/ProductOfArrayExceptSelf.java
+++ programs/src/com/Leetcode/ProductOfArrayExceptSelf.java
@@ -15,6 +15,39 @@ class Solution {
         //     ansArr[i] = ans;
         // }
 
+        // found it
+        // [1,2,3,4]
+        //
+        // calc prefix_mult = [1,2,6,24]
+        // calc suffix_mult = [24,24,12,4]
+        // if i = 0; ans[i] = suffix_mult[i + 1];
+        // if i = nums.length - 1; ans[i] = prefix_mult[i-1];
+        // else
+        // ans[i] = prefix_mult[i-1] * suffix_mult[i+1];
+
+      // O(N)
+        int[] prefix_mult = new int[n];
+        int[] suffix_mult = new int[n];
+        prefix_mult[0] = nums[0];
+        suffix_mult[n-1] = nums[n-1];
+
+        for(int i = 1, j = n - 2; i <= n-1 && j >= 0; i++, j--){
+            prefix_mult[i] = prefix_mult[i-1] * nums[i];
+            suffix_mult[j] = suffix_mult[j + 1] * nums[j];
+        }
+        System.out.println(Arrays.toString(prefix_mult));
+        System.out.println(Arrays.toString(suffix_mult));
+
+        for(int k = 0; k < n; k++){
+            if(k == 0) {
+                ansArr[k] = suffix_mult[k+1];
+            } else if( k == n - 1) {
+                ansArr[k] = prefix_mult[k-1];
+            } else {
+                ansArr[k] = prefix_mult[k-1] * suffix_mult[k+1];
+            }
+        }
+
         return ansArr;
     }
 }
