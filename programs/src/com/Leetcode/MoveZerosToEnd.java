fb9b2d0a0906921ac11d6d322c4c31ea9c9e4d70
Vivek Vishal <vishalvivek488@gmail.com>
2025-01-21 12:20:20 +0530

    Update MoveZerosToEnd.java

--- programs/src/com/Leetcode/MoveZerosToEnd.java
+++ programs/src/com/Leetcode/MoveZerosToEnd.java
@@ -1,18 +1,39 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j = 0;
-       for(int i = 0; i < nums.length; i++){
-        if(nums[i] == 0){
+    //     int j = 0;
+    //    for(int i = 0; i < nums.length; i++){
+    //     if(nums[i] == 0){
+    //         j = i;
+    //         while(j < nums.length){
+    //             if(nums[j] != 0){
+    //                  nums[i] = nums[j];
+    //                  nums[j] = 0;
+    //                  break;
+    //             }
+    //             j++;
+    //         }
+    //     }
+    //    }
+    // two pointer approach
+    int j = -1;
+    for(int i = 0; i < nums.length; i++){
+        if(nums[i] == 0) {
             j = i;
-            while(j < nums.length){
-                if(nums[j] != 0){
-                     nums[i] = nums[j];
-                     nums[j] = 0;
-                     break;
-                }
-                j++;
-            }
+            break;
         }
-       }
+    }
+    if(j == -1){ // no zeros in the nums, return it as is
+        return ;
+    }
+
+    for(int i = j + 1; i < nums.length; i++){
+        if(nums[i] != 0){
+            // swap jth and ith ele
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            j++;
+        }
+    }
     }
 }
